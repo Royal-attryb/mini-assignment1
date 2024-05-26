5b532081bd41e56d5df9860f969bde7f6c45b3fa
Royal-attryb <royal@attryb.com>
2024-05-26 15:16:28 +0530

    added basic authentication

--- proj1/src/index.js
+++ proj1/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Auth0Provider domain="dev-r3ogiikbu1jzu0vf.us.auth0.com" clientId="XMfbWKYGw7pCTyabBobVNp5xSGygCUd9" authorizationParams={{
+        redirect_uri: window.location.origin
+      }}>
+          <App />
+      </Auth0Provider>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
